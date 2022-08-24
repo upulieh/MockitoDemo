391c4ee7d4c502e75f7e968aadc3c1c9fa7a46d5
IT18107074 - Upulie H.D.I <IT18107074@my.sliit.lk>
2022-08-24 17:16:26 +0530

    Initial commit

---
+++ mockito-course/src/test/java/com/upulie/mockito/FirstMockitoTest.java
@@ -0,0 +1,13 @@
+package com.upulie.mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstMockitoTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+}
